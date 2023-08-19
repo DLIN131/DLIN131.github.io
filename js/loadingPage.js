060c5a185e16ab6a6145571eab3bca0aaa363756
DLIN131 <daiyulin945@gmail.com>
2023-08-19 22:56:44 +0800

    ver1.1.1

---
+++ js/loadingPage.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    // 載入完成後隱藏動畫元素
+    let loadingContainer = document.querySelector('.loading-container');
+    loadingContainer.style.display = 'none';
+
+  });
